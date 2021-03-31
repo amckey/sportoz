a9b7649d6c71774bc7da8c536b2f69e47b45317b
Arturs <zaharova@Arturss-MacBook-Air.local>
2021-03-31 19:51:32 +0300

    31

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    StatisticsComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot([
+      {path: '', component: HomepageComponent},
+      {path: 'statistics', component: StatisticsComponent},
+      {path: 'contacts', component: ContactsComponent},
+      {path: '**', component: HomepageComponent},
+        ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
